cf33ff077af11bdeee275b68a122fb1d52db2534
Czral <ngasp@hotmail.com>
2018-02-25 21:42:44 +0200

    Second

--- app/src/main/java/com/example/android/musicapp/MainActivity.java
+++ app/src/main/java/com/example/android/musicapp/MainActivity.java
@@ -1,55 +1,73 @@
 package com.example.android.musicapp;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.media.MediaMetadata;
+import android.media.MediaMetadataRetriever;
+import android.media.MediaPlayer;
+import android.media.MediaScannerConnection;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.io.File;
+import java.net.URI;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    ArrayList<Song> songs;
-
+    MediaPlayer song;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        song = MediaPlayer.create(MainActivity.this, R.raw.master_of_puppets);
 
         ImageView play = findViewById(R.id.play);
         play.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                songs = new ArrayList<>();
+                song.start();
 
-                songs.add(new Song("Freezing Moon", "De Mysteriis Dom Sathanas", "Mayhem", 1993, 2));
+                if (song.isPlaying()) {
 
-                songs.add(new Song("Exercises In Futility VI", "Exercises In Futility", "Mgla", 2015, 6));
-
-                songs.add(new Song("Wound Upon Wound", "Ad Majorem Sathanas Gloriam", "Gorgoroth", 2006, 1));
-
-
-
-                SongAdapter adapter = new SongAdapter(MainActivity.this, songs);
-
-                ListView songList = (ListView) findViewById(R.id.list_songs);
-
-                songList.setAdapter(adapter);
-
-                return;
-
+                    song.start();
+                }
 
             }
         });
 
+
+        ImageView pause = findViewById(R.id.pause);
+        pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                song.pause();
+            }
+        });
+
+        ImageView stop = findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                song.stop();
+                song.reset();
+                song.start();
+            }
+        });
 
 
         ImageView playlist = findViewById(R.id.playlist);
@@ -58,11 +76,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 Intent playlistIntent = new Intent(MainActivity.this, PlaylistActivity.class);
-                playlistIntent.putExtra("SONGS", songs);
                 startActivity(playlistIntent);
+
             }
         });
 
+        TextView titleSong = findViewById(R.id.title_song);
+        titleSong.setText("Title: Master Of Puppets");
+
+        TextView albumSong = findViewById(R.id.album_song);
+        albumSong.setText("Album: Master Of Puppets");
 
     }
 
--- app/src/main/java/com/example/android/musicapp/PlaylistActivity.java
+++ app/src/main/java/com/example/android/musicapp/PlaylistActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.musicapp;
 
+import android.net.wifi.p2p.WifiP2pManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 
@@ -14,6 +16,20 @@ public class PlaylistActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlist);
 
+        songs = new ArrayList<>();
+
+        songs.add(new Song("Master Of Puppets", "Master Of Puppets", "Metallica", 1986, 2));
+
+        songs.add(new Song("Exercises In Futility VI", "Exercises In Futility", "Mgla", 2015, 6));
+
+        songs.add(new Song("Wound Upon Wound", "Ad Majorem Sathanas Gloriam", "Gorgoroth", 2006, 1));
+
+
+        SongAdapter adapter = new SongAdapter(PlaylistActivity.this, songs);
+
+        ListView songList = (ListView) findViewById(R.id.list_song);
+
+        songList.setAdapter(adapter);
 
 
 
