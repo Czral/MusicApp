d19ab5198353ec69996bcba013ec5dea84a8ff61
Czral <ngasp@hotmail.com>
2018-02-26 19:52:58 +0200

    Fourth

--- app/src/main/java/com/example/android/musicapp/MusicBuyingActivity.java
+++ app/src/main/java/com/example/android/musicapp/MusicBuyingActivity.java
@@ -1,7 +1,11 @@
 package com.example.android.musicapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -18,7 +22,13 @@ public class MusicBuyingActivity extends AppCompatActivity {
 
         musicLink = new ArrayList<>();
 
-        musicLink.add(new MusicBuy("AMAZON", "https://www.amazon.com/MP3-Music-Download/b/ref=sa_menu_dmusic2?ie=UTF8&node=163856011"));
+        musicLink.add(new MusicBuy("AMAZON", "http://www.amazon.com/MP3-Music-Download/b/ref=sa_menu_dmusic2?ie=UTF8&node=163856011"));
+        musicLink.add(new MusicBuy("eMUSIC", "http://www.emusic.com/"));
+        musicLink.add(new MusicBuy("BLEEP", "http://bleep.com/"));
+        musicLink.add(new MusicBuy("BOOMKAT", "http://boomkat.com/"));
+        musicLink.add(new MusicBuy("NAPSTER", "http://gr.napster.com/"));
+        musicLink.add(new MusicBuy("TRACK IT DOWN", "http://www.trackitdown.net/"));
+
 
         MusicBuyAdapter adapterMusic = new MusicBuyAdapter(MusicBuyingActivity.this, musicLink);
 
@@ -26,10 +36,23 @@ public class MusicBuyingActivity extends AppCompatActivity {
 
         musicLinkList.setAdapter(adapterMusic);
 
+        musicLinkList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
-    }
+                if (i == 0) {
+
+                    Intent openLink = new Intent(Intent.ACTION_VIEW);
+                    openLink.setData(Uri.parse("http://www.amazon.com/MP3-Music-Download/b/ref=sa_menu_dmusic2?ie=UTF8&node=163856011"));
+                    startActivity(openLink);
+                }
+            }
+        });
 
 
 
+
+
+}
 
 }
